a6fcdb4a0a05519f438031b074fca7fa035fd7de
Hafiz <komefumi@protonmail.ch>
2023-09-10 02:34:55 +0530

    Allows excerpt and block editor

--- university-post-types.php
+++ university-post-types.php
@@ -7,9 +7,9 @@ Plugin Name: University Post Types
 function university_post_types()
 {
   register_post_type('event', array(
-    'rewrite' => array(
-      'slug' => 'events',
-    ),
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
     // 'show_in_rest' => true,
