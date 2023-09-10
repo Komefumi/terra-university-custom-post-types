6542112fc71dcbf77c16ebf2e20863054476a2dd
Hafiz <komefumi@protonmail.ch>
2023-09-10 14:55:35 +0530

    Creates program post type

--- university-post-types.php
+++ university-post-types.php
@@ -6,8 +6,9 @@ Plugin Name: University Post Types
 
 function university_post_types()
 {
+  // Event Post Type
   register_post_type('event', array(
-    'show_in_rest' => true,
+    // 'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
@@ -21,6 +22,24 @@ function university_post_types()
       'singular_name' => 'Event',
     ),
     'menu_icon' => 'dashicons-calendar-alt',
+  ));
+
+  // Program Post Type
+  register_post_type('program', array(
+    // 'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program',
+    ),
+    'menu_icon' => 'dashicons-awards',
   ));
 }
 
